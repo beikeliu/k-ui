cfc6d08d7ec7570b13566019f43c8148cace1907
v_beileiliu <v_beileiliu@tencent.com>
2022-10-21 15:44:08 +0800

    kcheckbox受控

--- src/docs/kcheckbox/index.tsx
+++ src/docs/kcheckbox/index.tsx
@@ -1,12 +1,28 @@
-import { KCheckbox } from "../../lib/components/kcheckbox";
+import { useState } from "react";
+import { KCheckbox, KCheckboxGroup } from "../../lib/components/kcheckbox";
+import { GroupProps } from "../../lib/components/kcheckbox/types";
 
 export const KCheckboxDoc: React.FC = () => {
+  const [value, setValue] = useState<GroupProps["value"]>(["1"]);
+  const onChange: GroupProps["onChange"] = (e) => {
+    if (e.target.checked) {
+      setValue([...value, e.target.id]);
+    } else {
+      const index = value.indexOf(e.target.id);
+      const valueCopy = [...value];
+      valueCopy.splice(index, 1);
+      setValue(valueCopy);
+    }
+  };
   return (
     <>
-      <KCheckbox>A</KCheckbox>
-      <KCheckbox>B</KCheckbox>
-      <KCheckbox>C</KCheckbox>
-      <KCheckbox>D</KCheckbox>
+      <KCheckboxGroup onChange={onChange} value={value}>
+        <KCheckbox value="1">A</KCheckbox>
+        <KCheckbox value="2">B</KCheckbox>
+        <KCheckbox value="3">C</KCheckbox>
+        <KCheckbox value="4">D</KCheckbox>
+      </KCheckboxGroup>
+      &nbsp;&nbsp;{value}
     </>
   );
 };
--- src/lib/components/kcheckbox/index.tsx
+++ src/lib/components/kcheckbox/index.tsx
@@ -1,13 +1,32 @@
 import "./index.css";
-import { Props } from "./types";
+import { Props, GroupProps } from "./types";
 
-export const KCheckbox: React.FC<Props> = ({ children }: Props) => {
+let checked: string[];
+let handleChange: GroupProps["onChange"];
+
+export const KCheckbox: React.FC<Props> = ({ children, value }: Props) => {
   return (
     <>
       <label className="k-checkbox">
-        <input type="checkbox" className="k-checkbox-input" />
+        <input
+          type="checkbox"
+          className="k-checkbox-input"
+          id={value}
+          checked={checked.includes(value)}
+          onChange={handleChange}
+        />
         {children}
       </label>
     </>
   );
+};
+
+export const KCheckboxGroup: React.FC<GroupProps> = ({
+  children,
+  value,
+  onChange,
+}: GroupProps) => {
+  checked = value;
+  handleChange = onChange;
+  return <>{children}</>;
 };
