4462b572b9fda6b8074bd0675b53b579153009e5
liubeike <liubeike99@gmail.com>
2022-11-22 17:18:14 +0800

    github add url

--- src/layout/head/index.tsx
+++ src/layout/head/index.tsx
@@ -1,8 +1,14 @@
 import styles from "./index.module.css";
 import github from "../../icons/github.svg";
+const URL = "https://github.com/beikeliu/k-ui";
 export const Head: React.FC = () => (
   <header className={styles.head}>
     <h1 className={styles.logo}>k-ui</h1>
-    <img className={styles.github} src={github} alt="" />
+    <img
+      className={styles.github}
+      src={github}
+      alt=""
+      onClick={() => window.open(URL)}
+    />
   </header>
 );
